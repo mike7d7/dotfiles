d81b7a0b46c9e5ccc57dd84349d8dbbbeae2ba97
mike7d7 <mike7d7@proton.me>
2024-11-03 17:44:09 -0600

    add firefox profile on ram

--- packages.nix
+++ packages.nix
@@ -1,12 +1,38 @@
 { config, pkgs, lib, ... }:
 let
-   dock-script = pkgs.writeShellScriptBin "dock-sh" ''
+  dock-script = pkgs.writeShellScriptBin "dock-sh" ''
     niri msg output eDP-1 off
     niri msg output DP-1 vrr on
   '';
   undock-script = pkgs.writeShellScriptBin "undock-sh" ''
    niri msg output eDP-1 on
    niri msg output eDP-1 vrr on
+  '';
+  firefox-sync = pkgs.writeShellScriptBin "firefox-sync" ''
+    static=static-$1
+    link=$1
+    volatile=/dev/shm/firefox-$1-$USER
+
+    IFS=
+    set -efu
+
+    cd ~/.mozilla/firefox
+
+    if [ ! -r $volatile ]; then
+    	mkdir -m0700 $volatile
+    fi
+
+    if [ "$(readlink $link)" != "$volatile" ]; then
+	    mv $link $static
+	    ln -s $volatile $link
+    fi
+
+    if [ -e $link/.unpacked ]; then
+	    rsync -av --delete --exclude .unpacked ./$link/ ./$static/
+    else
+	    rsync -av ./$static/ ./$link/
+	    touch $link/.unpacked
+    fi
   '';
   aagl = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz");
   navicat = pkgs.callPackage /home/mig/nixpkgs/pkgs/by-name/na/navicat-premium/package.nix {};
@@ -83,8 +109,21 @@ in
     ollama-cuda
     ciscoPacketTracer8
     navicat
+    rsync
 
     dock-script
     undock-script
+    firefox-sync
   ];
+  systemd.user.services.firefox-profile-memory-cache = {
+    description = "Firefox profile memory cache";
+    wantedBy = [ "default.target" ];
+    path = [pkgs.rsync];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = ''${firefox-sync}/bin/firefox-sync 0shu6evv.default'';
+      ExecStop = ''${firefox-sync}/bin/firefox-sync 0shu6evv.default'';
+    };
+  };
 }
