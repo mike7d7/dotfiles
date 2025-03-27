02e084a8ff60441b2a6e14d57909a4a68e8b91dc
mike7d7 <mike7d7@proton.me>
2025-03-27 17:18:23 -0600

    add pr version of swayosd as workaround for issue with playerctl

--- home-config.nix
+++ home-config.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... }:
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  swayosd-fixed = pkgs.swayosd.overrideAttrs (oldAttrs: {
+    src = pkgs.fetchFromGitHub {
+      owner = "soraxas";
+      repo = "SwayOSD";
+      rev = "20f834e1e563c603d456b2e5da05a4e65b6e3e10";
+      sha256 = "sha256-GuzFqaHl7W5Vrjalb0Wl6iKmY/xWZ7Dbuyo0X7NN7R4=";
+    };
+  });
 in
 {
   imports = [
@@ -20,6 +28,7 @@ in
     programs.zed-editor.enable = true;
     gtk.enable = true;
     services.swayosd.enable = true;
+    services.swayosd.package = swayosd-fixed;
 
     # Configs
     programs.git = {
