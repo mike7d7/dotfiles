c33ffdf0ce9954ef2e1be98573aebd4c7c47a194
mike7d7 <mike7d7@proton.me>
2025-05-18 09:55:36 -0600

    configuration.nix: set 'preempt=full' in the kernel

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,10 @@
   services.scx.enable = true;
   services.scx.scheduler = "scx_lavd"; # default is "scx_rustland"
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = [ "nowatchdog" ];
+  boot.kernelParams = [
+    "nowatchdog"
+    "preempt=full"
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
