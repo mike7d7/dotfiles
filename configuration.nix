9fcd5193d6b4554d57a28ae0cb1ae9aeb82563d2
mike7d7 <mike7d7@proton.me>
2025-05-18 09:54:31 -0600

    configuration.nix: minor fixes to pipewire low latency quantum value

--- configuration.nix
+++ configuration.nix
@@ -141,9 +141,9 @@
   services.pipewire.extraConfig.pipewire."92-low-latency" = {
     "context.properties" = {
       "default.clock.rate" = 48000;
-      "default.clock.quantum" = 64;
-      "default.clock.min-quantum" = 64;
-      "default.clock.max-quantum" = 64;
+      "default.clock.quantum" = 76;
+      "default.clock.min-quantum" = 76;
+      "default.clock.max-quantum" = 76;
     };
   };
   services.pipewire.wireplumber.extraConfig."10-bluez" = {
