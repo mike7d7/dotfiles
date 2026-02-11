b04d60736f14111249d8dca791ced7d75fd4f47e
mike7d7 <mike7d7@proton.me>
2026-02-10 21:19:40 -0600

    configuration.nix: enable openvpn

--- configuration.nix
+++ configuration.nix
@@ -32,7 +32,12 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-openvpn
+    ];
+  };
   networking.useDHCP = false;
   systemd.network = {
     enable = true;
