0a7a5c0b93f234b99b4e52553c8fd23765f41d1d
mike7d7 <mike7d7@proton.me>
2026-03-22 10:13:51 -0600

    flake.nix: add garnix binary cache

--- flake.nix
+++ flake.nix
@@ -42,6 +42,10 @@
             nixpkgs.overlays = [
               nix-cachyos-kernel.overlays.pinned
             ];
+            nix.settings.substituters = [ "https://cache.garnix.io" ];
+            nix.settings.trusted-public-keys = [
+              "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+            ];
           }
           ./configuration.nix
           home-manager.nixosModules.home-manager
